f83a1ee2d1194745d7e5ee284175596bf4bd92fe
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-03-19 16:30:26 +0800

    update deps

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/FloatTech/zbputils/control"
-	"github.com/FloatTech/zbputils/control/order"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
+
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
 )
 
 func Inita() {
 	// --------------------------在此下书写插件内容--------------------------
-	en := control.Register("demo", order.AcquirePrio(), &control.Options{
+	en := control.Register("demo", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
 		Help:             "help from demo",
 	})
 	en.OnCommand("demo", zero.AdminPermission).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			fmt.Println("msg recv.")
 			ctx.SendChain(message.Text("回复"))
 		})
 	// --------------------------在此上书写插件内容--------------------------
